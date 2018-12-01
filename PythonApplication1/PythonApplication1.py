558d3fab9e5eb4a6f1a143e6536e898498f35f00
altamirano123 <altamirano1531@gmail.com>
2018-12-01 13:05:01 -0600

    Add project files.

---
+++ PythonApplication1/PythonApplication1.py
@@ -0,0 +1,24 @@
+
+
+a = "hello world"
+print(a)
+
+x =2
+y = x + 5 
+
+print(y)
+
+Student_name = "Sheila"
+print(Student_name[0])
+print(Student_name[1])
+print(Student_name[2])
+
+print(Student_name[2:5])
+
+print(Student_name[1:])
+
+print(a[5:])
+
+word="Isaac"
+for x in word:
+    print(x)
